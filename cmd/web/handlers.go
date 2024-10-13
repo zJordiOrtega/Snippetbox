c9f8f8e8e614bd95a81baae72cc72ac16a98cf0a
zJordiOrtega <jimenezjordi020@gmail.com>
2024-10-13 16:47:32 -0700

    base template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -9,10 +11,27 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
     http.NotFound(w, r)
+    return
   }
   
-  w.Write([]byte("Hello from Snippetbox"))
-}
+  files := []string {
+    "./ui/html/base.html",
+    "./ui/html/pages/home.html",
+  }
+
+  ts, err := template.ParseFiles(files...)
+  if err != nil {
+    log.Print(err.Error())
+    http.Error(w, "Internal Server Error", 500)
+    return
+  }
+
+  err = ts.ExecuteTemplate(w, "base", nil)
+  if err != nil {
+    log.Print(err.Error())
+    http.Error(w, "Internal Server Error", 500)
+  }
+ }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
   id, err := strconv.Atoi(r.URL.Query().Get("id"))
