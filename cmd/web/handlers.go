ede6b17c5c4a9535eb3677bfe75a3213c4fd6596
zJordiOrtega <jimenezjordi020@gmail.com>
2024-10-13 16:57:11 -0700

    css and javascript added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,6 +17,7 @@ func home(w http.ResponseWriter, r *http.Request) {
   files := []string {
     "./ui/html/base.html",
     "./ui/html/pages/home.html",
+    "./ui/html/partials/nav.html",
   }
 
   ts, err := template.ParseFiles(files...)
