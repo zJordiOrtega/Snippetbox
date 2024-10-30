7e15f49932ec49bdb92edb9512586180e1a7b870
zJordiOrtega <jimenezjordi020@gmail.com>
2024-10-29 22:55:21 -0700

    database connection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"snippetbox.zjordi.net/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +15,16 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
+
+  snippets, err := app.snippets.Latest()
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
+
+  for _, snippet := range snippets{
+    fmt.Fprintf(w, "%+v\n", snippet)
+  }
 
 	files := []string{
 		"./ui/html/base.html",
@@ -38,7 +51,16 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+  snippet, err := app.snippets.Get(id)
+  if err != nil {
+    if errors.Is(err, models.ErrNoRecord) {
+      app.notFound(w)
+    } else {
+      app.serverError(w, err)
+    }
+    return
+  }
+  fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
